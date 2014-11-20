c212a5f4c3594176f510c1cf1926f3205bb9053e
Ryoma ABE <arayahomuhomu@gmail.com>
2014-11-21 02:27:51 +0900

    Stopできるようになった

--- js/timer.js
+++ js/timer.js
@@ -1,27 +1,33 @@
+  second=0; 
+  minute=5;
+
 function Timer(){
- second=0; 
- minute=2;
- timerID=setInterval(function(){
- if(second<0 && minute > 0){
-         second=59;
-         minute--;
-  }
-  minuteString = (minute<10)?"0"+minute : minute;
-  secondString = (second<10)?"0"+second : second;
-  remainingtime=minuteString + ":" + secondString;
-  
-  document.getElementById("countdown").innerText=remainingtime;
-    
-  if(second==0 && minute==0){
-   dora.play();
-   dispAlert();
-   
-   clearInterval(timerID);
-  }
-  second--;
-},1000);
+ 
+  timerID=setInterval(function(){
+    if(second<0 && minute > 0){
+      second=59;
+      minute--;
+    }
+    minuteString = (minute<10)?"0"+minute : minute;
+    secondString = (second<10)?"0"+second : second;
+    remainingtime=minuteString + ":" + secondString;
+
+    document.getElementById("countdown").innerText=remainingtime;
+
+    if(second==0 && minute==0){
+      dora.play();
+      dispAlert();
+
+      clearInterval(timerID);
+    }
+    second--;
+  },1000);
 }
 
 function dispAlert(){
-window.alert("Time Up");
+  window.alert("Time Up");
+}
+
+function Stop(){
+clearInterval(timerID);
 }
