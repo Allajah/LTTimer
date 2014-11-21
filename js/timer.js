584fde8a61b1e48df2632aeb92824a0351dc5068
Ryoma ABE <arayahomuhomu@gmail.com>
2014-11-21 09:14:50 +0900

    indication 1/100sec

--- js/timer.js
+++ js/timer.js
@@ -1,27 +1,32 @@
   second=0; 
   minute=5;
-
+  minisecond=0;
 function Timer(){
  
   timerID=setInterval(function(){
+    if(minisecond<0 ){
+    minisecond=99;
+    second--;
+    }
     if(second<0 && minute > 0){
       second=59;
       minute--;
     }
+    minisecString = (minisecond<10)?"0"+minisecond : minisecond;
     minuteString = (minute<10)?"0"+minute : minute;
     secondString = (second<10)?"0"+second : second;
-    remainingtime=minuteString + ":" + secondString;
+    remainingtime=minuteString + ":" + secondString + ":" + minisecString;
 
     document.getElementById("countdown").innerText=remainingtime;
 
-    if(second==0 && minute==0){
+    if(minisecond==0 && second==0 && minute==0){
       dora.play();
       dispAlert();
 
       clearInterval(timerID);
     }
-    second--;
-  },1000);
+    minisecond--;
+  },10);
 }
 
 function dispAlert(){
